a099afa3f7a6a47cbdcac6e1ea4b0ef849ee14a7
FlArEs <paul2314@naver.com>
2018-04-18 17:30:19 +0900

    Add Mobile Project

---
+++ CNMAL Mobile/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CNMAL_Mobile {
+	[Activity(Label = "CNMAL_Mobile", MainLauncher = true)]
+	public class MainActivity : Activity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
