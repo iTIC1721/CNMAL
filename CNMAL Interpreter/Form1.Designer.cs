cbd7dbb52e8fe16434645112f8fc3d4dce392855
FlArEs <paul2314@naver.com>
2018-04-18 17:20:03 +0900

    CNMAL

---
+++ CNMAL Interpreter/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CNMAL_Interpreter {
+	partial class Form1 {
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent() {
+			this.inputBox = new System.Windows.Forms.TextBox();
+			this.OutputBox = new System.Windows.Forms.TextBox();
+			this.resetButton = new System.Windows.Forms.Button();
+			this.compileButton = new System.Windows.Forms.Button();
+			this.LineCountLabel = new System.Windows.Forms.Label();
+			this.letterCountLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// inputBox
+			// 
+			this.inputBox.Location = new System.Drawing.Point(12, 41);
+			this.inputBox.Multiline = true;
+			this.inputBox.Name = "inputBox";
+			this.inputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.inputBox.Size = new System.Drawing.Size(760, 355);
+			this.inputBox.TabIndex = 0;
+			this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+			// 
+			// OutputBox
+			// 
+			this.OutputBox.Location = new System.Drawing.Point(12, 403);
+			this.OutputBox.Multiline = true;
+			this.OutputBox.Name = "OutputBox";
+			this.OutputBox.ReadOnly = true;
+			this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.OutputBox.Size = new System.Drawing.Size(760, 146);
+			this.OutputBox.TabIndex = 1;
+			// 
+			// resetButton
+			// 
+			this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.resetButton.Location = new System.Drawing.Point(12, 12);
+			this.resetButton.Name = "resetButton";
+			this.resetButton.Size = new System.Drawing.Size(75, 23);
+			this.resetButton.TabIndex = 2;
+			this.resetButton.Text = "Reset";
+			this.resetButton.UseVisualStyleBackColor = true;
+			this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+			// 
+			// compileButton
+			// 
+			this.compileButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.compileButton.Location = new System.Drawing.Point(697, 12);
+			this.compileButton.Name = "compileButton";
+			this.compileButton.Size = new System.Drawing.Size(75, 23);
+			this.compileButton.TabIndex = 3;
+			this.compileButton.Text = "Compile";
+			this.compileButton.UseVisualStyleBackColor = true;
+			this.compileButton.Click += new System.EventHandler(this.compileButton_Click);
+			// 
+			// LineCountLabel
+			// 
+			this.LineCountLabel.AutoSize = true;
+			this.LineCountLabel.Location = new System.Drawing.Point(93, 9);
+			this.LineCountLabel.Name = "LineCountLabel";
+			this.LineCountLabel.Size = new System.Drawing.Size(0, 12);
+			this.LineCountLabel.TabIndex = 4;
+			this.LineCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// letterCountLabel
+			// 
+			this.letterCountLabel.AutoSize = true;
+			this.letterCountLabel.Location = new System.Drawing.Point(93, 23);
+			this.letterCountLabel.Name = "letterCountLabel";
+			this.letterCountLabel.Size = new System.Drawing.Size(0, 12);
+			this.letterCountLabel.TabIndex = 5;
+			this.letterCountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.letterCountLabel);
+			this.Controls.Add(this.LineCountLabel);
+			this.Controls.Add(this.compileButton);
+			this.Controls.Add(this.resetButton);
+			this.Controls.Add(this.OutputBox);
+			this.Controls.Add(this.inputBox);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(800, 600);
+			this.MinimumSize = new System.Drawing.Size(800, 600);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "CNMAL";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox inputBox;
+		private System.Windows.Forms.TextBox OutputBox;
+		private System.Windows.Forms.Button resetButton;
+		private System.Windows.Forms.Button compileButton;
+		private System.Windows.Forms.Label LineCountLabel;
+		private System.Windows.Forms.Label letterCountLabel;
+	}
+}
+
---
+++ CNMAL Interpreter/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace CNMAL_Interpreter {
+
+	#region Enum
+	public enum Exception {
+		NonNeedWordsException,
+		RunOutOfStackException,
+		NonFormCommentException
+	}
+	#endregion
+
+	public partial class Form1 : Form {
+
+		#region Database
+		Stack<int> mainStack;
+		Dictionary<string, int> dividePoint;
+		#endregion
+
+		bool isQuited = false;
+
+		#region Form Methods
+		public Form1() {
+			InitializeComponent();
+			mainStack = new Stack<int>();
+			dividePoint = new Dictionary<string, int>();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			TextUpdate();
+		}
+
+		private void inputBox_TextChanged(object sender, EventArgs e) {
+			TextUpdate();
+		}
+
+		private void TextUpdate() {
+			LineCountLabel.Text = "줄 수: " + inputBox.Lines.Length.ToString();
+			letterCountLabel.Text = "글자 수: " + inputBox.Text.Length.ToString();
+		}
+
+		// 리셋 버튼 클릭
+		private void resetButton_Click(object sender, EventArgs e) {
+			inputBox.Text = "";
+			OutputBox.Text = "";
+		}
+
+		// 컴파일 버튼 클릭
+		private void compileButton_Click(object sender, EventArgs e) {
+			if (inputBox.Text.Trim() != "")
+				Build(inputBox.Lines);
+		}
+		#endregion
+
+		#region Interpret Methods
+		/// <summary>
+		/// inputBox에 있는 코드를 빌드한다.
+		/// </summary>
+		/// <param name="inputArray"></param>
+		private void Build(string[] inputArray) {
+			// 초기화
+			isQuited = false;
+			OutputBox.Text = "";
+			mainStack.Clear();
+
+			string[] contentArray = RemoveComment(inputArray);
+
+			if (!SearchNeedWords(contentArray))
+				Throw("...", Exception.NonNeedWordsException);
+
+			
+			Interpret(contentArray);
+		}
+
+		/// <summary>
+		/// 프로그램이 필요한 단어를 갖추고 있는지 확인한다.
+		/// </summary>
+		/// <param name="textArray"></param>
+		/// <returns></returns>
+		private bool SearchNeedWords(string[] textArray) {
+			bool[] result = new bool[3];
+			foreach (string text in textArray) {
+				if (text.Contains("자율")) {
+					result[0] = true;
+					break;
+				}
+			}
+			foreach (string text in textArray) {
+				if (text.Contains("창의")) {
+					result[1] = true;
+					break;
+				}
+			}
+			foreach (string text in textArray) {
+				if (text.Contains("품격")) {
+					result[2] = true;
+					break;
+				}
+			}
+			return result[0] && result[1] && result[2];
+		}
+
+		/// <summary>
+		/// 문자열에서 주석 부분(소괄호 내부)을 제거한다.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="commentRemovedText"></param>
+		/// <returns></returns>
+		private string[] RemoveComment(string[] textArray) {
+			string[] resultArray = textArray;
+			for(int i = 0; i < textArray.Length; i++) {
+				int cnsaCount = 0;
+				if (isContainCNSANet(textArray[i], out cnsaCount))
+					for (int j = 0; j < cnsaCount; j++)
+						ConnectCNSANet();
+
+				if (textArray[i].Contains("(") || textArray[i].Contains(")")) {
+					if ((textArray[i].Contains("(") && textArray[i].Contains(")")) && (textArray[i].LastIndexOf(')') > textArray[i].LastIndexOf('(') && textArray[i].IndexOf('(') < textArray[i].IndexOf(')'))) {
+						string firstString = textArray[i].Substring(0, textArray[i].IndexOf("("));
+						string lastString = textArray[i].Substring(textArray[i].LastIndexOf(")") + 1, textArray[i].Length - (textArray[i].LastIndexOf(")") + 1));
+						resultArray[i] = firstString + lastString;
+					}
+					else {
+						Throw("...", Exception.NonFormCommentException);
+					}
+				}
+			}
+			return resultArray;
+		}
+
+		/// <summary>
+		/// 문장을 해석해 결과를 출력합니다
+		/// </summary>
+		/// <param name="textArray"></param>
+		private void Interpret(string[] text) {
+			for (int i = 0; i < text.Length; i++) {
+				int cnsaCount = 0;
+				if (isContainCNSANet(text[i], out cnsaCount))
+					for (int j = 0; j < cnsaCount; j++)
+						ConnectCNSANet();
+
+				if (text[i].Contains("방울토마토")) {
+					mainStack.Clear();
+				}
+				else if (text[i].Contains("큰사넷")) {
+					if ((text[i].Contains("빠릅니다") || text[i].Contains("빠른")) && (text[i].Contains("느립니다") || text[i].Contains("느린"))) {
+						mainStack.Push(0);
+					}
+					else if (text[i].Contains("빠릅니다") || text[i].Contains("빠른")) {
+						mainStack.Push(1);
+					}
+					else if (text[i].Contains("느립니다") || text[i].Contains("느린")) {
+						mainStack.Push(-1);
+					}
+					else {
+						mainStack.Push(0);
+					}
+				}
+				else {
+					if (text[i].StartsWith("자율적인 큰사이언은 ")) {
+						mainStack.Push(text[i].Split(' ').Length - 2);
+					}
+					else if (text[i].StartsWith("창의적인 큰사이언은 ")) {
+						mainStack.Push(text[i].Replace(" ", "").Substring(9).Length);
+					}
+					else if (text[i].StartsWith("품격있는 큰사이언은 ")) {
+						int wordCount = text[i].Split(' ').Length - 2;
+						if (mainStack.Count >= wordCount) {
+							int multi = 1;
+							for (int j = 0; j < wordCount; j++) {
+								multi *= mainStack.Pop();
+							}
+							mainStack.Push(multi);
+						}
+						else {
+							Throw("...", Exception.RunOutOfStackException);
+						}
+					}
+					else if (text[i].StartsWith("지금은 ") && text[i].EndsWith("입니다")) {
+						if (!dividePoint.ContainsKey(text[i].Replace("지금은 ", "").Replace("입니다", "").Trim()))
+							dividePoint.Add(text[i].Replace("지금은 ", "").Replace("입니다", "").Trim(), i);
+					}
+					else if (text[i].Contains("친구들은 ")) {
+						string[] removedText = text[i].Split(new string[] { "친구들이 말하기를 " }, StringSplitOptions.None);
+						int[] wordCount = { removedText[0].Trim().Split(' ').Length, removedText[1].Split(' ').Length };
+						if (mainStack.Count >= wordCount[0]) {
+							int value = GetStackIndexOf(mainStack, wordCount[0]);
+							for (int j = 0; j < wordCount[1]; j++) {
+								mainStack.Push(value);
+							}
+						}
+						else {
+							Throw("...", Exception.RunOutOfStackException);
+						}
+					}
+					else if (text[i].Contains("친구들이 말하기를 ")) {
+						string[] removedText = text[i].Split(new string[] { "친구들이 말하기를 " }, StringSplitOptions.None);
+						int wordCount = removedText[1].Split(' ').Length;
+						if (mainStack.Count >= wordCount) {
+							int sum = 0;
+							for (int j = 0; j < wordCount; j++) {
+								sum += mainStack.Pop();
+							}
+							Print(sum.ToString());
+						}
+						else {
+							Throw("...", Exception.RunOutOfStackException);
+						}
+					}
+					else if (text[i].Contains("선생님은 ")) {
+						string[] removedText = text[i].Split(new string[] { "선생님은 " }, StringSplitOptions.None);
+						int wordCount = removedText[1].Split(' ').Length;
+						if (mainStack.Count >= wordCount) {
+							int[] nums = new int[wordCount];
+							for (int j = 0; j < wordCount; j++) {
+								nums[i] = -1 * mainStack.Pop();
+							}
+							for (int j = wordCount - 1; j >= 0; j--) {
+								mainStack.Push(nums[i]);
+							}
+						}
+						else {
+							Throw("...", Exception.RunOutOfStackException);
+						}
+					}
+					else if (text[i].Contains("선생님께서 말씀하시기를 ")) {
+						string[] removedText = text[i].Split(new string[] { "선생님께서 말씀하시기를 " }, StringSplitOptions.None);
+						int wordCount = removedText[1].Split(' ').Length;
+						if (mainStack.Count >= wordCount) {
+							int sum = 0;
+							for (int j = 0; j < wordCount; j++) {
+								sum += mainStack.Pop();
+							}
+							Print((char)(sum));
+						}
+						else {
+							Throw("...", Exception.RunOutOfStackException);
+						}
+					}
+					else if (text[i].Contains("에는 ")) {
+						string[] removedText = text[i].Split(new string[] { "에는 " }, 2, StringSplitOptions.None);
+						if (removedText[1].Trim().Split(' ').Length == mainStack.Peek()) {
+							if (dividePoint.ContainsKey(removedText[0].Trim())) {
+								i = dividePoint[removedText[0].Trim()];
+							}
+						}
+					}
+				}
+
+				if (text[i].Contains("겨레의 하나됨과")) {
+					Stream stream = Properties.Resources.교가;
+					SoundPlayer player = new SoundPlayer(stream);
+					player.Play();
+				}
+
+				if (text[i].Contains("로그아웃 되었습니다")) {
+					Quit();
+					return;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 문장에 큰사넷이 포함되어 있는지 확인한다
+		/// </summary>
+		/// <param name="text">확인할 문장</param>
+		/// <param name="count">등장한 개수</param>
+		/// <returns></returns>
+		private bool isContainCNSANet(string text, out int count) {
+			count = 0;
+			int temp = GetWordCount(text, "큰사넷", false)
+				+ GetWordCount(text, "CNSANet", false)
+				+ GetWordCount(text, "CNSA Net", false);
+
+			if (temp == 0)
+				return false;
+
+			count = temp;
+			return true;
+		}
+
+		/// <summary>
+		/// 큰사넷과 연결을 시도한다
+		/// 사실 아무것도 안한다
+		/// </summary>
+		private void ConnectCNSANet() {
+			for (int i = 0; i < 5000000; i++) {
+				string cnsa = "http://10.1.100.32/";
+				string home = cnsa + "home";
+				if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) {
+					// 홈 플러스 가격이 착해
+					string uri = home + "plus";
+				}
+			}
+		}
+		#endregion
+
+		#region Output Methods
+		/// <summary>
+		/// 코드의 실행 결과를 출력한다
+		/// </summary>
+		/// <param name="text"></param>
+		private void Print(char text) {
+			if (!isQuited) {
+				OutputBox.Text += text.ToString();
+			}
+		}
+
+		/// <summary>
+		/// 코드의 실행 결과를 출력한다
+		/// </summary>
+		/// <param name="text"></param>
+		private void Print(string text) {
+			if (!isQuited) {
+				OutputBox.Text += text;
+			}
+		}
+
+		/// <summary>
+		/// 코드의 실행 결과를 출력한다
+		/// </summary>
+		/// <param name="text"></param>
+		private void Print(string[] textArray) {
+			if (!isQuited) {
+				string text = string.Join("\r\n", textArray);
+				OutputBox.Text += text;
+			}
+		}
+
+		/// <summary>
+		/// 예외를 출력한다
+		/// </summary>
+		/// <param name="exceptionText"></param>
+		/// <param name="type"></param>
+		private void Throw(string exceptionText, Exception type) {
+			switch (type) {
+				case Exception.NonNeedWordsException:
+					Print(exceptionText);
+					Print("\r\n\r\n프로그램이 품격을 갖추지 않았습니다.\r\n제거 대상입니다.");
+					inputBox.Text = "";
+					isQuited = true;
+					break;
+
+				case Exception.RunOutOfStackException:
+					Print(exceptionText);
+					isQuited = true;
+					break;
+
+				case Exception.NonFormCommentException:
+					Print(exceptionText);
+					isQuited = true;
+					break;
+			}
+		}
+
+		/// <summary>
+		/// 프로그램을 정상적으로 종료한다.
+		/// </summary>
+		private void Quit() {
+			Print("\r\n\r\n프로그램이 정상적으로 종료되었습니다.");
+			isQuited = true;
+		}
+		#endregion
+
+		#region Utility Methods
+		private int GetWordCount(string source, string target, bool exact) {
+			int count = 0;
+			string sourceText = exact ? source : source.ToLower();
+			string targetText = exact ? target : target.ToLower();
+			for (int i = 0; i < source.Length - target.Length + 1; i++)
+				if (source.Substring(i, target.Length).Equals(target))
+					count++;
+			return count;
+		}
+
+		private T GetStackIndexOf<T>(Stack<T> stack, int index) {
+			T result = default(T);
+			for (int i = 0; i < index; i++) {
+				result = stack.Pop();
+			}
+			return result;
+		}
+		#endregion
+	}
+}
